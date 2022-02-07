e29547ddd4a0fcf43b903c951cabdd2cfa02a6d4
Maksim Denischik <m.denischik@pokermatch.com>
2022-02-07 22:39:10 +0200

    fixed bot

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 const { Telegraf } = require('telegraf')
-const { MenuTemplate, MenuMiddleware } = require('telegraf-inline-menu')
 const xpquest = require('./scripts/xpquest-send')
 const materialsSend = require('./scripts/materials-send')
 const materialsTransfer = require('./scripts/materials-transfer')
@@ -10,46 +9,26 @@ const bot = new Telegraf(process.env.BOT_TOKEN, { handlerTimeout: 9_000_000 }) /
 
 if (!process.env.BOT_TOKEN) throw new Error('"BOT_TOKEN" env var is required!')
 
-bot.command('xpquest', (ctx) => {
-	ctx.reply('Starting...')
-	;(async () => {
-		const result = await xpquest.sendXpQuest()
-		ctx.reply(`${result}`)
-	})().catch((err) => {
-		console.error(err)
-	})
-})
+// V3.0.0
 
-bot.command('materialssend', (ctx) => {
-	ctx.reply('Starting...')
-	;(async () => {
-		const result = await materialsSend.sendMaterialsFarming()
-		ctx.reply(`${result}`)
-	})().catch((err) => {
-		console.error(err)
-	})
-})
-
-bot.command('materialstransfer', (ctx) => {
-	ctx.reply('Starting...')
-	;(async () => {
-		const result = await materialsTransfer.transferMaterials()
-		ctx.reply(`${result}`)
-	})().catch((err) => {
-		console.error(err)
-	})
-})
-
-bot.command('count', (ctx) => {
-	ctx.reply('Starting...')
-	;(async () => {
-		const result = await countAdventurers.countAdventurers()
-		ctx.reply(`${result}`)
-	})().catch((err) => {
-		console.error(err)
-	})
-})
-
+const inline_menu = {
+	reply_markup: {
+		one_time_keyboard: false,
+		inline_keyboard: [
+			[{ text: '🔄 Re-count my adventurers', callback_data: 'recount' }],
+			[{ text: '🏋🏻 Send adventurers to farm XP', callback_data: 'xpfarm' }],
+			[{ text: '🧱 Send adventurers to farm materials', callback_data: 'materialsfarm' }],
+			[{ text: '📦 Transfer materials to crafter', callback_data: 'materialstransfer' }],
+			[{ text: '📞 Summon new adventurer', callback_data: 'summon' }],
+		],
+	},
+}
+const menu = {
+	reply_markup: {
+		one_time_keyboard: false,
+		keyboard: [[{ text: 'Menu' }]],
+	},
+}
 bot.command('retrieve', (ctx) => {
 	ctx.reply('Starting...')
 	;(async () => {
@@ -59,10 +38,47 @@ bot.command('retrieve', (ctx) => {
 		console.error(err)
 	})
 })
-
-bot.command('summon', (ctx) => {
-	ctx.reply('Starting...')
+bot.command('menu', (ctx) => ctx.reply('Choose action:', menu))
+bot.hears('Menu', (ctx) => ctx.reply('Choose action:', inline_menu))
+bot.action('recount', (ctx) => {
 	;(async () => {
+		ctx.reply('Starting...')
+		const result = await countAdventurers.countAdventurers()
+		ctx.reply(`${result}`)
+	})().catch((err) => {
+		console.error(err)
+	})
+})
+bot.action('xpfarm', (ctx) => {
+	;(async () => {
+		ctx.reply('Sending...')
+		const result = await xpquest.sendXpQuest()
+		ctx.reply(`${result}`)
+	})().catch((err) => {
+		console.error(err)
+	})
+})
+bot.action('materialsfarm', (ctx) => {
+	;(async () => {
+		ctx.reply('Starting...')
+		const result = await materialsSend.sendMaterialsFarming()
+		ctx.reply(`${result}`)
+	})().catch((err) => {
+		console.error(err)
+	})
+})
+bot.action('materialstransfer', (ctx) => {
+	;(async () => {
+		ctx.reply('Starting...')
+		const result = await materialsTransfer.transferMaterials()
+		ctx.reply(`${result}`)
+	})().catch((err) => {
+		console.error(err)
+	})
+})
+bot.action('summon', (ctx) => {
+	;(async () => {
+		ctx.reply('Starting...')
 		const result = await summonAdventurer.summonAdventurer()
 		ctx.reply(`${result}`)
 	})().catch((err) => {
@@ -70,54 +86,6 @@ bot.command('summon', (ctx) => {
 	})
 })
 
-const menuTemplate = new MenuTemplate((ctx) => `Hey ${ctx.from.first_name}!`)
-
-menuTemplate.interact('🔄 Re-count my adventurers', 'a', {
-	do: async (ctx) => {
-		ctx.reply('Counting...')
-		const result = await countAdventurers.countAdventurers()
-		ctx.reply(`${result}`)
-		return false
-	},
-})
-menuTemplate.interact('🏋🏻 Send adventurers to farm XP', 'b', {
-	do: async (ctx) => {
-		ctx.reply('Sending...')
-		const result = await xpquest.sendXpQuest()
-		ctx.reply(`${result}`)
-		return false
-	},
-})
-menuTemplate.interact('🧱 Send adventurers to farm materials', 'c', {
-	do: async (ctx) => {
-		ctx.reply('Sending...')
-		const result = await materialsSend.sendMaterialsFarming()
-		ctx.reply(`${result}`)
-		return false
-	},
-})
-
-menuTemplate.interact('📦 Transfer materials to crafter', 'd', {
-	do: async (ctx) => {
-		ctx.reply('Sending...')
-		const result = await materialsTransfer.transferMaterials()
-		ctx.reply(`${result}`)
-		return false
-	},
-})
-
-menuTemplate.interact('📞 Summon new adventurer', 'e', {
-	do: async (ctx) => {
-		ctx.reply('Sending...')
-		const result = await summonAdventurer.summonAdventurer()
-		ctx.reply(`${result}`)
-		return false
-	},
-})
-
-const menuMiddleware = new MenuMiddleware('/', menuTemplate)
-bot.command('menu', (ctx) => menuMiddleware.replyToContext(ctx))
-bot.use(menuMiddleware)
 bot.launch()
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
